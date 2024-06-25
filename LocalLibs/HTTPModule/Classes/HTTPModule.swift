749b6df873e2cccc08d0e8ed192cd4723ccce8e8
Aventador <ws419460185@gmail.com>
2024-06-25 11:44:31 +0800

    fix the bug that prevents the client from receiving a response

--- LocalLibs/HTTPModule/Classes/HTTPModule.swift
+++ LocalLibs/HTTPModule/Classes/HTTPModule.swift
@@ -10,11 +10,15 @@ import SwiftProtobuf
 import Alamofire
 
 public struct Http {
-    static let urlString = "http://8.140.207.230:8080";
+//    static let urlString = "http://127.0.0.1:8080"
+    static let urlString = "http://8.140.207.230:8080"
+    
     static let url = URL(string: urlString)!
     
-    public static func post<T:Message, U:Message>(_ request: T, responseType: U.Type, finish: @escaping (Result<U,AFError>) -> Void) {
-        let reqSerializer = ProtobufRequestSerializer(message: request)
+    
+    
+    public static func post<T:Message, U:Message>(_ cmd: PHM_cmd, payload: T, responseType: U.Type, finish: @escaping (Result<U,AFError>) -> Void) {
+        let reqSerializer = ProtobufRequestSerializer(message: payload, cmd: cmd)
         let respSerializer = ProtobufResponseSerializer<U>()
         AF.request(reqSerializer).response(responseSerializer: respSerializer) { response in
             finish(response.result)
@@ -24,12 +28,20 @@ public struct Http {
     
     struct ProtobufRequestSerializer<T:Message>: URLRequestConvertible {
         let message: T
+        let cmd: PHM_cmd
         
         func asURLRequest() throws -> URLRequest {
             var request = URLRequest(url:url)
             request.setValue("application/x-protobuf", forHTTPHeaderField: "Content-Type")
             request.httpMethod = HTTPMethod.post.rawValue
-            request.httpBody = try? message.serializedData()
+            
+            var transport = PHM_request.with{
+                $0.cmd = cmd
+                if let payload = try? Google_Protobuf_Any(message: message) {
+                    $0.payload = payload
+                }
+            }
+            request.httpBody = try? transport.serializedData()
             return request
         }
     }
@@ -40,7 +52,9 @@ public struct Http {
         func serialize(request: URLRequest?, response: HTTPURLResponse?, data: Data?, error: (any Error)?) throws -> SerializedObject {
             guard error == nil else { throw error! }
             guard let data else { throw AFError.responseValidationFailed(reason: .dataFileNil) }
-            return try T(serializedData: data)
+            
+            var transport = try PHM_response(serializedData: data)
+            return try T(unpackingAny: transport.payload)
         }
     }
 }
